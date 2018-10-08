9a8a5570dbcb5f3f392b27c5e86eddc6b8e899ef
Lucca-Falbo <luccafalbo@gmail.com>
2018-10-08 11:58:06 -0300

    Adicionando NavBar

--- tattoo/src/App.js
+++ tattoo/src/App.js
@@ -1,12 +1,46 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand, NavbarNav, NavbarToggler, Collapse, NavItem, NavLink } from 'mdbreact';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './index.css';
 
-class Main extends Component {
-	render() {
-		return (
-			<div>
-			</div>
-			)
-	}
+
+class App extends Component {
+  constructor(props) {
+        super(props);
+        this.state = {
+            collapse: false,
+            isWideEnough: false,
+        };
+    this.onClick = this.onClick.bind(this);
+    }
+
+    onClick(){
+        this.setState({
+            collapse: !this.state.collapse,
+        });
+    }
+    render() {
+        return (
+            <Router>
+                <Navbar color="indigo" dark expand="md" scrolling>
+                    <NavbarBrand href="/">
+                        <strong>Estudios BH</strong>
+                    </NavbarBrand>
+                    { !this.state.isWideEnough && <NavbarToggler onClick = { this.onClick } />}
+                    <Collapse isOpen = { this.state.collapse } navbar>
+                        <NavbarNav left>
+                          <NavItem>
+                              <NavLink to="#">Inkonik</NavLink>
+                          </NavItem>
+                          <NavItem>
+                              <NavLink to="#">Pieta</NavLink>
+                          </NavItem>
+                        </NavbarNav>
+                    </Collapse>
+                </Navbar>
+            </Router>
+        );
+    }
 }
 
-export default Main;
+export default App;
